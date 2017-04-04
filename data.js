dd6ab8e93c00b7f2ed9e64c493b6eb2e5e802025
apertureone <mahan.nekoui@gmail.com>
2017-04-04 17:12:12 -0400

    data.js is a skeleton file for holding the data

---
+++ data.js
@@ -0,0 +1,28 @@
+// Global lists for storage
+
+var cuts = [
+	
+];
+
+var clipps = [
+	
+];
+
+var feedbacks = [
+	
+];
+
+// Profile object to which we will add from the global lists
+var profile = {
+	barbername = "me",
+	
+	cuts:[
+		
+	],
+	clipps:[
+		
+	],
+	feedbacks:[
+		
+	],
+}
